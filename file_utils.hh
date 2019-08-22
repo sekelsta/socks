e110e811f02795930e90ff6367841ee78029129b
Tiffany Jager <tjager@caltech.edu>
2019-08-22 17:08:55 -0400

    file IO bugfix

--- file_utils.hh
+++ file_utils.hh
@@ -7,6 +7,7 @@ using namespace std;
 
 inline void file_shift(int start, int new_start, FILE *file) {
     assert(new_start > start);
+    assert (file != nullptr);
     // Allocate and set string end
     char *buffer_first = (char *)malloc(new_start - start + 1);
     buffer_first[new_start - start] = '\0';
@@ -25,15 +26,17 @@ inline void file_shift(int start, int new_start, FILE *file) {
     
     int read_location = start + bytes_read;
     int write_location = new_start;
-    while (!feof(file)) {
+    bool done = feof(file);
+    while (!done) {
         // Read into second buffer
         if (fseek(file, read_location, SEEK_SET) != 0) {
             throw "TODO: deal with file errors";
         }
-        bytes_read = fread(buffer_first, sizeof(char), new_start - start, file);
+        bytes_read = fread(buffer_second, sizeof(char), new_start - start, file);
         if (bytes_read != new_start - start && !feof(file)) {
             throw "TODO: deal with file errors";
         }
+        done = feof(file);
         read_location += bytes_read;
         // Write from first buffer
         if (fseek(file, write_location, SEEK_SET) != 0) {
